f52a5a56b6a4c70694eb316e5784de4f04f1ca8c
Benedikt Huber <benedikt.huber@gmail.com>
2014-02-04 10:11:56 +0100

    Patch system.h for new libc++ (ported gold.h patch from: FreeBSD Ports, r325806) - clang-specific code to check whether __has_include(<unordered_set>) - CXX11-specific definitions for unordered set,map and multimap

--- gold/system.h
+++ gold/system.h
@@ -56,8 +56,27 @@
 #endif
 
 // Figure out how to get a hash set and a hash map.
+#ifdef __clang__
+#if __has_include(<unordered_set>)
+#define HAVE_CXX11_UNORDERED_SET 1
+#define HAVE_TR1_UNORDERED_SET
+#define HAVE_TR1_UNORDERED_MAP
+#endif
+#endif
+#if HAVE_CXX11_UNORDERED_SET
 
-#if defined(HAVE_TR1_UNORDERED_SET) && defined(HAVE_TR1_UNORDERED_MAP) \
+#include <unordered_set>
+#include <unordered_map>
+
+// We need a template typedef here.
+
+#define Unordered_set std::unordered_set
+#define Unordered_map std::unordered_map
+#define Unordered_multimap std::unordered_multimap
+
+#define reserve_unordered_map(map, n) ((map)->rehash(n))
+
+#elif defined(HAVE_TR1_UNORDERED_SET) && defined(HAVE_TR1_UNORDERED_MAP) \
     && defined(HAVE_TR1_UNORDERED_MAP_REHASH)
 
 #include <tr1/unordered_set>
